57105e4a157ff496ca63b07289511f2cf0314816
Blake Johnson <blake.k.johnson.4@gmail.com>
2026-07-03 09:01:56 -0400

    fix failing treesitter loading

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "master",
   build = ":TSUpdate",
   config = function()
-    local config = require("nvim-treesitter.config")
+    local config = require("nvim-treesitter.configs")
 
     config.setup({
       ensure_installed = { "lua", "markdown", "typescript", "vim" },
